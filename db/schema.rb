1d452daa0cd025ba5781f362aa3520819c2dc891
David Sanderson <coderdave@gmail.com>
2022-04-25 10:43:43 -0400

    fixing recipes description column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_24_223600) do
+ActiveRecord::Schema.define(version: 2022_04_25_142927) do
 
   create_table "chefs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "chefname"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2022_04_24_223600) do
 
   create_table "recipes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
