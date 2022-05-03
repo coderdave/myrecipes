70ca3869bc19c745e6de0ec4a25ac78115898a25
David Sanderson <coderdave@gmail.com>
2022-05-02 21:12:09 -0400

    adding admin attribute

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,7 @@
 class ChefsController < ApplicationController
   before_action :set_chef, only: [:show, :edit, :update, :destroy]
   before_action :require_same_user, only: [:edit, :update, :destroy]
-  # before_action :require_admin, only: [:destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 5)
@@ -40,11 +40,11 @@ class ChefsController < ApplicationController
   end
   
   def destroy
-    # if !@chef.admin?
+    if !@chef.admin?
       @chef.destroy
       flash[:danger] = "Chef and all associated recipes have been deleted!"
       redirect_to chefs_path
-    # end
+    end
   end
   
   private
@@ -58,7 +58,7 @@ class ChefsController < ApplicationController
   end
   
   def require_same_user
-    if current_chef != @chef # and !current_chef.admin?
+    if current_chef != @chef and !current_chef.admin?
       flash[:danger] = "You can only edit or delete your own account"
       redirect_to chefs_path
     end
