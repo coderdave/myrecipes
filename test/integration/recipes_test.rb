8236d42f389864437af23409421d4ec8e8e81ff3
Faustino <faustino.garcia@agilethought.com>
2022-05-25 11:40:11 -0500

    chefname column of chef table changed to name

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class RecipesTest < ActionDispatch::IntegrationTest
   def setup
-    @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com",
+    @chef = Chef.create!(name: "mashrur", email: "mashrur@example.com",
       password: "password", password_confirmation: "password")
     @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee, add vegetable and oil", chef: @chef)
     @recipe2 = @chef.recipes.build(name: "chicken saute", description: "great chicken dish")
@@ -27,7 +27,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_template 'recipes/show'
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
-    assert_match @chef.chefname, response.body
+    assert_match @chef.name, response.body
     assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
     assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
     assert_select 'a[href=?]', recipes_path, text: "Return to recipes listing"
