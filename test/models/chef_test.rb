ecaefb49994d72a213cad02cd67e0f490965b398
Jose Luis <montalgex18@gmail.com>
2022-05-25 15:22:15 -0500

    TDD for likes model

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -2,34 +2,34 @@ require 'test_helper'
 
 class ChefTest < ActiveSupport::TestCase
   def setup
-    @chef = Chef.new(chefname: "mashrur", email: "mashrur@example.com",
+    @chef = Chef.new(chefname: "mashrur",     email: "mashrur@example.com",
       password: "password", password_confirmation: "password")
-end
-  
+  end
+
   test "should be valid" do
     assert @chef.valid?
   end
-  
+
   test "name should be present" do
     @chef.chefname = " "
     assert_not @chef.valid?
   end
-  
+
   test "name should be less than 30 characters" do
     @chef.chefname = "a" * 31
     assert_not @chef.valid?
   end
-  
+
   test "email should be present" do
     @chef.email = " "
     assert_not @chef.valid?
   end
-  
+
   test "email should not be too long" do
     @chef.email = "a" * 245 + "@example.com"
     assert_not @chef.valid?
   end
-  
+
   test "email should accept correct format" do
     valid_emails = %w[user@example.com MASHRUR@gmail.com M.first@yahoo.ca john+smith@co.uk.org]
     valid_emails.each do |valids|
@@ -37,14 +37,14 @@ end
       assert @chef.valid?, "#{valids.inspect} should be valid"
     end
   end
-  
+
   test "should reject invalid addresses" do
     invalid_emails = %w[mashrur@example mashrur@example,com mashrur.name@gmail. joe@bar+foo.com]
     invalid_emails.each do |invalids|
       @chef.email = invalids
       assert_not @chef.valid?, "#{invalids.inspect} should be invalid"
     end
-  end 
+  end
 
   test "email should be unique and case insensitive" do
     duplicate_chef = @chef.dup
@@ -57,14 +57,14 @@ end
     mixed_email = "JohN@ExampLe.com"
     @chef.email = mixed_email
     @chef.save
-    assert_equal mixed_email.downcase, @chef.reload.email 
+    assert_equal mixed_email.downcase, @chef.reload.email
   end
 
   test "password should be present" do
     @chef.password = @chef.password_confirmation = " "
     assert_not @chef.valid?
   end
-  
+
   test "password should be atleast 5 character" do
     @chef.password = @chef.password_confirmation = "x" * 4
     assert_not @chef.valid?
---
+++ test/models/recipe_likes.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class RecipeLikes < ActiveSupport::TestCase
+
+  def setup
+    @chef = Chef.create!(chefname: "AtDev", email: "mashrur@example.com", password: "password", password_confirmation: "password")
+    @recipe = Recipe.create!(name:"apples", description: "Any description", chef_id: @chef.id )
+    @recipe_likes = RecipeLike.new(recipes_id: @recipe.id,chef_id: @chef.id, like: true)
+  end
+
+  test "should be valid" do
+    assert @recipe_likes.valid?
+  end
+
+  test "recipes id should be present" do
+    @recipe_likes.recipes_id = " "
+    assert_not @recipe_likes.valid?
+  end
+
+  test "chef id should be present" do
+    @recipe_likes.chef_id = " "
+    assert_not @recipe_likes.valid?
+  end
+
+  test "like without chef should be invalid" do
+    @recipe_likes.chef_id = nil
+    assert_not @recipe_likes.valid?
+  end
+
+  test "like without recipe should be invalid" do
+    @recipe_likes.recipes_id = nil
+    assert_not @recipe_likes.valid?
+  end
+
+end
