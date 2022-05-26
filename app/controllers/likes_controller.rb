aca9d6d07a3bd6f8e4efc813dd99e39e35309cc7
Alejandro Ramos <aramosbengy@gmail.com>
2022-05-26 11:58:38 -0500

    Create a new like action in controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,30 @@
 class LikesController < ApplicationController
+before_action :set_recipe, only: [:show, ]
+
   def show
   end
 
-  def new
-  end
-
   def create
+    @like = Like.new(like: true, recipe_id: recipe_params)
+    @like.chef = current_chef
+    if @like.save
+      flash[:success] = "like was created successfully"
+      redirect_to recipes_path
+    else
+      flash[:danger] = "Sorry I dont like it"
+    end
   end
 
-  def delete
+  def update
+
   end
+
+  private
+    def set_likes
+      @likes = Like.all
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:id)
+    end
 end
