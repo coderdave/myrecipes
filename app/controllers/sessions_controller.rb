f362aef914cda12a3cf25df13fd66a943cab1804
Chema Martinez <martinezchemamx@yahoo.com.mx>
2022-05-26 11:24:33 -0500

    send email and review on preview page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
   
   def new
-    
+    @accountVerified = params[:account_verified]
+    byebug
   end
   
   def create
     chef = Chef.find_by(email: params[:session][:email].downcase)
-    if chef && chef.authenticate(params[:session][:password])
+    byebug
+    #[:emailVerified]
+    if chef && chef.authenticate(params[:session][:password]) && params[:account_verified]
+      chef.account_verified = true
+      chef.save
+      session[:chef_id] = chef.id
+      cookies.signed[:chef_id] = chef.id
+      flash[:success] = "You have successfully logged in and verified your email account"
+      redirect_to chef
+    elsif chef && chef.authenticate(params[:session][:password])
       session[:chef_id] = chef.id
       cookies.signed[:chef_id] = chef.id
       flash[:success] = "You have successfully logged in"
