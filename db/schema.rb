ad20a7bdc75b87f44229f0fee19b464d10677e30
Jose Luis <montalgex18@gmail.com>
2022-05-26 16:33:35 -0500

    deleted like column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_132409) do
+ActiveRecord::Schema.define(version: 2022_05_26_212926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2022_05_26_132409) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.boolean "like"
     t.integer "chef_id"
     t.integer "recipe_id"
     t.datetime "created_at", precision: 6, null: false
