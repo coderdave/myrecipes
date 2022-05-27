5e51184715714abfb130c91db1ad275735666e15
Faustino <faustino.garcia@agilethought.com>
2022-05-27 15:33:16 -0500

    Ingredients delete test added to integration tests

---
+++ test/integration/ingredients_delete_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class IngredientsDeleteTest < ActionDispatch::IntegrationTest
+  def setup
+    @admin = Chef.create!(name:"Admin",email:"Admin@gmail.com",password:"password",password_confirmation:"password",admin:true) 
+    @recipe = Recipe.create(name:"Tacos de cochinita",description:"Long and usefull description", chef: @admin)
+    @ingredient = Ingredient.create!(name:"Pork meat")
+    @recipe.ingredients << @ingredient
+  end
+  
+  test "successfully delete an ingredient" do
+    sign_in_as(@chef, "password")
+    get edit_ingredient_path(@ingredient)
+    assert_template 'ingredients/edit'
+    assert_select 'a[href=?]', edit_ingredient_path(@ingredient), text: "Delete ingredient"
+    assert_difference 'Ingredient.count', -1 do
+      delete ingredient_path(@ingredient)
+    end
+    assert_redirected_to ingredients_path
+    assert_not flash.empty?
+  end
+  
+end
