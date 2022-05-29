2ee7e0ad5d540a0ee53d6903836041cc7a656a9e
Faustino <faustino.garcia@agilethought.com>
2022-05-28 19:23:13 -0500

    pluralize for delete ingredient flash added

--- test/integration/ingredients_delete_test.rb
+++ test/integration/ingredients_delete_test.rb
@@ -5,7 +5,9 @@ class IngredientsDeleteTest < ActionDispatch::IntegrationTest
     @admin = Chef.create!(name:"Admin",email:"Admin@gmail.com",password:"password",password_confirmation:"password",admin:true) 
     @recipe = Recipe.create!(name:"Tacos de cochinita",description:"Long and usefull description", chef: @admin)
     @ingredient = Ingredient.create!(name:"Pork meat")
+    @ingredient_2= Ingredient.create!(name:"Tortillas")
     @recipe.ingredients << @ingredient
+    @recipe.ingredients << @ingredient_2
   end
   
   test "successfully delete an ingredient" do
@@ -18,6 +20,12 @@ class IngredientsDeleteTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to ingredients_path
     assert_not flash.empty?
+  end
+
+  test "Succesfully delete the association between recipe and ingredient after deleting a recipe" do
+    sign_in_as(@admin, "password")
+    delete recipe_path(@recipe)
+    assert RecipeIngredient.where(recipe_id: @recipe).count == 0
   end
   
 end
