3e18ce4aa4249eaef580afa80d1258fe710155fd
Carlos <j.morin@ukko.mx>
2022-05-30 18:15:24 -0500

    Add new scope to get menu items

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,6 @@ class Ingredient < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
+
+  scope :menu_items, -> { order(created_at: :desc).limit(5) }
 end
