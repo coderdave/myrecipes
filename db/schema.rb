7609ad1a69f77ce8f182991fd49a99861a394a90
Faustino <faustino.garcia@agilethought.com>
2022-05-31 07:58:01 -0500

    schema.rb updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_161946) do
+ActiveRecord::Schema.define(version: 2022_05_25_213358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
