ef89dfb6a58530a5d823abeacf9e86f4ced0278d
Chema Martinez <martinezchemamx@yahoo.com.mx>
2022-05-31 16:13:27 -0500

    adding tests for welcome email and email verification with token

--- test/mailers/chef_mailer_test.rb
+++ test/mailers/chef_mailer_test.rb
@@ -1,11 +1,18 @@
 require "test_helper"
 
 class ChefMailerTest < ActionMailer::TestCase
-  # test "welcome_email" do
-  #   mail = PostMailer.post_created
-  #   assert_equal "Post created", mail.subject
-  #   assert_equal ["to@example.org"], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+
+  def setup
+    @chef = Chef.create!(name: "mashrur", email: "mashrur@example.com", password: "password")
+  end
+
+  test "welcome email created for new chef" do
+    mail = ChefMailer.with(chef: @chef).welcome_email
+    assert_equal "Please Verify MyRecipes Account", mail.subject
+    assert_equal [@chef.email], mail.to
+    assert_equal ["smyrecipes@gmail.com"], mail.from
+    assert_match "Welcome "+@chef.name+" to MyRecipes the best recipe sharing app ever!", mail.body.encoded
+    assert_match @chef.auth_token, mail.body.encoded
+  end
+
 end
--- test/mailers/previews/chef_mailer_preview.rb
+++ test/mailers/previews/chef_mailer_preview.rb
@@ -1,9 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/chef_mailer
 class ChefMailerPreview < ActionMailer::Preview
     def welcome_email
-      #chef = current_chef
-      #@url  = 'http://localhost/login'
-      #byebug
       ChefMailer.with(chef: Chef.last).welcome_email
     end
 end
