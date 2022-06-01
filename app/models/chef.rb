2bc36d3b7cb78ac8aaba9a57b2ecf32dc56dc28b
cdelacruz8512 <cruz.delacruz@agilethought.com>
2022-06-01 09:26:03 -0500

    fix remove set_auth_token and generate_token

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -12,22 +12,8 @@ class Chef < ApplicationRecord
   validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
    
   before_save { self.email = email.downcase }
-  before_create :set_auth_token
   
   has_secure_password
   has_secure_token :auth_token
-
-  private
-
-  def set_auth_token
-    self.auth_token = generate_token
-  end
-
-  def generate_token
-    loop do
-      token = SecureRandom.hex(10)
-      break token unless Chef.where(auth_token: token).exists?
-    end
-  end
   
 end
