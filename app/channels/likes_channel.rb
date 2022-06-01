7a7f92ff4a86762cc28b70f04c506cf2026a6bd3
Alejandro Ramos <aramosbengy@gmail.com>
2022-06-01 09:41:01 -0500

    Delete likes channel

--- app/channels/likes_channel.rb
+++
@@ -1,9 +0,0 @@
-class LikesChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "likes_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
--- test/channels/likes_channel_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class LikesChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
-end
