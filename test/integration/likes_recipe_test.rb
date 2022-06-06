ecf312cb400c853f26042cdf79d294d9ca9d50e7
Alejandro Ramos <aramosbengy@gmail.com>
2022-06-06 14:37:38 -0500

    Fix bug with integration tests

--- test/integration/likes_recipe_test.rb
+++ test/integration/likes_recipe_test.rb
@@ -7,27 +7,27 @@ class LikesRecipe < ActionDispatch::IntegrationTest
     @recipe = Recipe.create!(name: "vegetable saute", 
                             description: "great vegetable sautee, add vegetable and oil",
                             chef: @chef)
-    @like = Like.create!(recipe_id: @recipe.id, chef_id: @chef.id)
+    
   end
 
-  test "create a new like"
+  test "create a new like" do
     sign_in_as(@chef, "password")
-    get recipes_path
-    assert_template 'recipes/index'
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
     assert_difference 'Like.count', 1 do
-      post recipe_likes_path(@recipe), params: { like: { recipe_id: @recipe.id, chef_id: @chef.id } }
+      post recipe_likes_path(@recipe), xhr: true, params: { like: { recipe_id: @recipe.id, chef_id: @chef.id } }
     end
   end
 
-  test "delete a like"
+  test "delete a like" do
+    @like = Like.create!(recipe_id: @recipe.id, chef_id: @chef.id)
     sign_in_as(@chef, "password")
-    get recipe_path
-    assert_template 'recipe/index'
-    if @like.chef_id == @chef.id && @like.recipe_id == @recipe.id
-      assert_select 'a[href=?]', recipe_like_path(recipe_id: @recipe.id, id: @like.id)
-      assert_difference 'Like.count', -1 do
-        delete recipe_like_path(recipe_id: @recipe.id, id: @like.id)
-      end
+    get recipe_path(@recipe), xhr: true
+    assert_template 'recipes/show'
+    assert_select 'a[href=?]', recipe_like_path(recipe_id: @recipe.id, id: @like.id)
+    assert_difference 'Like.count', -1 do
+      delete recipe_like_path(recipe_id: @recipe.id, id: @like.id), xhr: true
     end
   end
+
 end
